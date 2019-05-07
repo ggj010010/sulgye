fbb15f1b9d7264e49e48a687a72dd5ea57e05c15
ggj010010 <49180696+ggj010010@users.noreply.github.com>
2019-05-07 15:54:03 +0900

    update(cell_combine)

--- minjae/src/minjae/ui/SchaduleUI.java
+++ minjae/src/minjae/ui/SchaduleUI.java
@@ -2,9 +2,13 @@ package minjae.ui;
 
 import java.awt.Component;
 import java.awt.EventQueue;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Vector;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
@@ -16,15 +20,16 @@ import javax.swing.JTable;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableModel;
 
 import com.toedter.calendar.JDateChooser;
 
 public class SchaduleUI extends JFrame {
 
 	private JPanel contentPane;
-	private JTable table;
-
+//	private JTable table;
+	final MultiSpanCellTable table;
+	final CellSpan cellAtt;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -54,14 +59,27 @@ public class SchaduleUI extends JFrame {
 		JScrollPane scrollPane = new JScrollPane();
 		
 		JButton btn_ScaduleAdd = new JButton("\uCD94\uAC00");
+		btn_ScaduleAdd.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				int[] columns = table.getSelectedColumns();
+				int[] rows = table.getSelectedRows();
+				cellAtt.combine(rows, columns);
+				table.clearSelection();
+				table.revalidate();
+				table.repaint();
+			}
+		});
 		
 		JDateChooser scaduleCalender = new JDateChooser();
+		scaduleCalender.setDate(new Date());
 		scaduleCalender.getDateEditor().addPropertyChangeListener(
 			    new PropertyChangeListener() {
 			        @Override
 			        public void propertyChange(PropertyChangeEvent e) {
 			            if ("date".equals(e.getPropertyName())) {
 			                System.out.println(e.getNewValue());
+			                System.out.println(scaduleCalender.getDate().toString());
 			            }
 			        }
 			    });
@@ -88,66 +106,110 @@ public class SchaduleUI extends JFrame {
 					.addContainerGap())
 		);
 		
-		table = new JTable();
+		Object[] c = new Object[] {
+				"10:30~11:00",
+				"11:00~11:30",
+				"11:30~12:00",
+				"12:00~12:30",
+				"12:30~13:00",
+				"13:00~13:30",
+				"13:30~14:00",
+				"14:00~14:30",
+				"14:30~15:00",
+				"15:00~15:30",
+				"15:30~16:00",
+				"16:00~16:30",
+				"16:30~17:00",
+				"17:00~17:30",
+				"17:30~18:00",
+				"18:00~18:30",
+				"18:30~19:00",
+				"19:00~19:30",
+				"19:30~"
+			};
+		
+		Vector dataColumnName = new Vector();
+		dataColumnName.addElement("\uC2DC\uAC04");
+		dataColumnName.addElement("\uC6D0\uC7A5");
+		dataColumnName.addElement("\uC870\uC324");
+//		System.out.println(dataColumnName);
+		
+		Vector rowData2 = new Vector();
+		for(int i =0;i<c.length;i++) {
+			Vector rowData = new Vector();
+			rowData.addElement(c[i]);
+			rowData.addElement(null);
+			rowData.addElement(null);
+			rowData2.addElement(rowData);
+//			System.out.println(rowData);
+			}
+//		System.out.println(rowData2);
+		
+		
+		AttributiveCellTableModel ml = new AttributiveCellTableModel(rowData2,dataColumnName);
+		cellAtt = (CellSpan) ml.getCellAttribute();
+		table = new MultiSpanCellTable(ml);
+		
+//		table = new JTable();
 		table.setColumnSelectionAllowed(true);
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
 		table.setRowHeight(25);
 		table.setFillsViewportHeight(true);
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
-				{"10:30~11:00", null, null},
-				{"11:00~11:30", null, null},
-				{"11:30~12:00", null, null},
-				{"12:00~12:30", null, null},
-				{"12:30~13:00", null, null},
-				{"13:00~13:30", null, null},
-				{"13:30~14:00", null, null},
-				{"14:00~14:30", null, null},
-				{"14:30~15:00", null, null},
-				{"15:00~15:30", null, null},
-				{"15:30~16:00", null, null},
-				{"16:00~16:30", null, null},
-				{"16:30~17:00", null, null},
-				{"17:00~17:30", null, null},
-				{"17:30~18:00", null, null},
-				{"18:00~18:30", null, null},
-				{"18:30~19:00", null, null},
-				{"19:00~19:30", null, null},
-				{"19:30~", null, null},
-			},
-			new String[] {
-				"\uC2DC\uAC04", "\uC6D0\uC7A5", "\uC870\uC324"
-			}
-		));
+//		table.setModel(new DefaultTableModel(
+//			new Object[][] {
+//				{"10:30~11:00", null, null},
+//				{"11:00~11:30", null, null},
+//				{"11:30~12:00", null, null},
+//				{"12:00~12:30", null, null},
+//				{"12:30~13:00", null, null},
+//				{"13:00~13:30", null, null},
+//				{"13:30~14:00", null, null},
+//				{"14:00~14:30", null, null},
+//				{"14:30~15:00", null, null},
+//				{"15:00~15:30", null, null},
+//				{"15:30~16:00", null, null},
+//				{"16:00~16:30", null, null},
+//				{"16:30~17:00", null, null},
+//				{"17:00~17:30", null, null},
+//				{"17:30~18:00", null, null},
+//				{"18:00~18:30", null, null},
+//				{"18:30~19:00", null, null},
+//				{"19:00~19:30", null, null},
+//				{"19:30~", null, null},
+//			},
+//			new String[] {
+//				"\uC2DC\uAC04", "\uC6D0\uC7A5", "\uC870\uC324"
+//			}
+//		));
 		table.getColumnModel().getColumn(0).setPreferredWidth(85);
 		table.getColumnModel().getColumn(0).setMaxWidth(100);
 		
-		table.getColumn("\uC2DC\uAC04").setCellRenderer(new SelectCellRenderer());
-		table.getColumn("\uC6D0\uC7A5").setCellRenderer(new SelectCellRenderer());
+//		table.getColumn("\uC2DC\uAC04").setCellRenderer(new SelectCellRenderer());
+//		table.getColumn("\uC6D0\uC7A5").setCellRenderer(new SelectCellRenderer());
 		
 		scrollPane.setViewportView(table);
 		contentPane.setLayout(gl_contentPane);
 	}
 	
-	class SelectCellRenderer extends DefaultTableCellRenderer {
-
-		public SelectCellRenderer() {
-
-			setOpaque(true);
-			setHorizontalAlignment(0);
-
-		}
-
-		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
-				int row, int column) {
-
-			Component cell = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-
-			if (isSelected && hasFocus)
-				System.out.println("I am cell in row " + row + " and column " + column);
-
-			return cell;
-
-		}
-	}
+//	class SelectCellRenderer extends DefaultTableCellRenderer {
+//
+//		public SelectCellRenderer() {
+//
+//			setOpaque(true);
+//			setHorizontalAlignment(0);
+//
+//		}
+//
+//		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+//				int row, int column) {
+//
+//			Component cell = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+//
+//			if (isSelected && hasFocus)
+//				System.out.println("I am cell in row " + row + " and column " + column);
+//
+//			return cell;
+//
+//		}
+//	}
 }
